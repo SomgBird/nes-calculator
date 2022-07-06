29d8222378e5fbd570a08a20d7f9ae15c4b1e512
SomgBird <dlarionov51@gmail.com>
2022-07-07 00:21:53 +0300

    test sprite drawing

--- main.s
+++ main.s
@@ -17,12 +17,45 @@ main:
     cpx #$20
     bne @pload
 
+    ldx #$00
+@sload:
+    lda sdata, X
+    sta $0200, X
+    inx
+    cpx #$40
+    bne @sload
+    cli
+    lda #%10000000  ; enable NMI
+    sta PPU_CTRL
+    lda #%00010000  ; enable sprites
+    sta PPU_MASK
+
+
 @loop:
     jmp @loop
 
+sdata:
+        ; X   №   a   Y
+    .byte $40, $00, $00, $08
+    .byte $40, $01, $00, $10
+    .byte $40, $02, $00, $18
+    .byte $40, $03, $00, $20
+    .byte $40, $04, $00, $28
+    .byte $40, $05, $00, $30
+    .byte $40, $06, $00, $38
+    .byte $40, $07, $00, $40
+    .byte $50, $08, $00, $08
+    .byte $50, $09, $00, $10
+    .byte $50, $0A, $00, $18
+    .byte $50, $0B, $00, $20
+    .byte $50, $0C, $00, $28
+    .byte $50, $0D, $00, $30
+    .byte $50, $0E, $00, $38
+    .byte $50, $0F, $00, $40
+
 pdata:
-    .byte $0F,$31,$32,$33,$0F,$35,$36,$37,$0F,$39,$3A,$3B,$0F,$3D,$3E,$0F  ;background palette data
-    .byte $33,$1C,$15,$14,$0F,$02,$38,$3C,$0F,$1C,$15,$14,$0F,$02,$38,$3C  ;sprite palette data
+    .byte $FF,$1A,$23,$37,$0F,$35,$36,$37,$0F,$39,$3A,$3B,$0F,$3D,$3E,$0F  ;background palette data
+    .byte $FF,$1C,$15,$14,$0F,$02,$38,$3C,$0F,$1C,$15,$14,$0F,$02,$38,$3C  ;sprite palette data
 
 .segment "OAM"
 
